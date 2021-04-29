756beb1dec3878f0188f859a6e43d65289ee73d1
dlnioanna <dln.ioanna@gmail.com>
2021-04-29 22:28:21 +0300

    before removing userUi

--- app/src/main/java/unipi/protal/vaccineappointment/FirebaseUIActivity.java
+++ app/src/main/java/unipi/protal/vaccineappointment/FirebaseUIActivity.java
@@ -1,13 +1,20 @@
 package unipi.protal.vaccineappointment;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import unipi.protal.vaccineappointment.databinding.ActivityFirebaseUiBinding;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -15,17 +22,60 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class FirebaseUIActivity extends AppCompatActivity {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class FirebaseUIActivity extends AppCompatActivity implements LocationListener {
     private static final int RC_SIGN_IN = 123;
+    private static final int REQUEST_LOCATION= 1000;
+    public static final String ANONYMOUS = "anonymous";
+    private static final String TAG = "MainActivity";
+    // Firebase instance variables
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ChildEventListener childEventListener;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private String mUsername;
+    private ActivityFirebaseUiBinding binding;
+    private LocationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firebase_ui);
+        binding = ActivityFirebaseUiBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        mUsername = ANONYMOUS;
+        // Initialize Firebase components
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("vaccine_point");
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    // User is signed out
+                    onSignedOutCleanup();
+                    createSignInIntent();
+                }
+            }
+        };
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        binding.signOutButton.setOnClickListener(v->signOut());
+        binding.findHospital.setOnClickListener(v->gps(v));
         createSignInIntent();
     }
 
@@ -43,7 +93,7 @@ public class FirebaseUIActivity extends AppCompatActivity {
                         .setLogo(R.drawable.ic_banner_virus)      // Set logo drawable
                         .setTheme(R.style.firebaseUIStyle)      // Set theme
                         .setAvailableProviders(providers)
-                        .setIsSmartLockEnabled(false)
+                        .setIsSmartLockEnabled(false)   //saves users credentials to phone device
                         .build(),
                 RC_SIGN_IN);
         // [END auth_fui_create_intent]
@@ -60,32 +110,122 @@ public class FirebaseUIActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Intent intent = new Intent(this,UserUI.class);
-                intent.putExtra("currentUser",user);
-                startActivity(intent);
-
+//                Intent intent = new Intent(this,UserUI.class);
+//                intent.putExtra("currentUser",user);
+//                startActivity(intent);
+                binding.userName.setText(user.getDisplayName());
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise show
                 // response.getError().getMessage().
-                Toast.makeText(this, response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
             }
         }
     }
     // [END auth_fui_result]
 
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+        // todo clear view
+        detachDatabaseReadListener();
+    }
+
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+        attachDatabaseReadListener();
+    }
+
+    private void attachDatabaseReadListener() {
+        if (childEventListener == null) {
+            childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    //todo otan prostheto astheni h allh efarmogh na ananeonetai
+                }
+
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                }
+
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                }
+
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                }
+
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            };
+            databaseReference.addChildEventListener(childEventListener);
+        }
+    }
     public void signOut() {
-        // [START auth_fui_signout]
         AuthUI.getInstance()
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
-                        Log.e("signo out","user signed out");
+                        onSignedOutCleanup();
                     }
                 });
-        // [END auth_fui_signout]
     }
 
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+        // todo clear view
+        detachDatabaseReadListener();
 
+    }
+
+    private void detachDatabaseReadListener() {
+        if (childEventListener != null) {
+            databaseReference.removeEventListener(childEventListener);
+            childEventListener = null;
+        }
+    }
+
+    private void gps(View view){
+        if(ActivityCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},REQUEST_LOCATION);
+        }else{
+            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUEST_LOCATION && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+            }
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
 
 }
